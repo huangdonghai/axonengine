7850c1ed8ecaec89940c25b0511b5395c8284136
hdh <hdh@axonengine.com>
2009-11-11 18:05:38 +0800

    as last

--- axengine/src/axeditorapp/trackwidget.cpp
+++ axengine/src/axeditorapp/trackwidget.cpp
@@ -1,12 +1,80 @@
 #include "trackwidget.h"
 
+namespace {
+
+	enum {
+		TRACK_HEIGHT = 32
+	};
+
+	static const QColor s_background(64,64,64);
+}
+
+TrackWidgetItem::TrackWidgetItem()
+{
+
+}
+
+TrackWidgetItem::TrackWidgetItem(TrackWidget* widget)
+{
+
+}
+
+TrackWidgetItem::TrackWidgetItem(TrackWidgetItem* parent)
+{
+
+}
+
+TrackWidgetItem::TrackWidgetItem(TrackWidgetItem* parent, Gfx::IAnimatable* anim)
+{
+
+}
+
+TrackWidgetItem::TrackWidgetItem(TrackWidgetItem* parent, TrackWidgetItem* preceding)
+{
+
+}
+
+TrackWidgetItem::~TrackWidgetItem()
+{
+
+}
+
+void TrackWidgetItem::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget /*= 0*/ )
+{
+	painter->drawText(0, 0, "track");
+	painter->drawRect(0, 0, 50, 50);
+	painter->drawText(0, 80, "track");
+}
+
+QRectF TrackWidgetItem::boundingRect() const
+{
+	return QRectF(0,0,1,1);
+}
+
 TrackWidget::TrackWidget(QWidget *parent)
 	: QGraphicsView(parent)
 {
+	m_scene = new QGraphicsScene();
+	setScene(m_scene);
 
+	TrackWidgetItem* item = new TrackWidgetItem();
+	m_scene->addItem(item);
+
+//	this->setBackgroundBrush(s_background);
+//	this->translate(100,100);
 }
 
 TrackWidget::~TrackWidget()
 {
 
+}
+
+void TrackWidget::resizeEvent(QResizeEvent * event)
+{
+	// make sure the top-left is in (0,0) pos
+	QSize size = event->size();
+	setSceneRect(0,0,size.width(),size.height());
+
+//	centerOn(size.width()/2, size.height()/2);
+//	centerOn(0,0);
 }
--- axengine/src/axeditorapp/trackwidget.h
+++ axengine/src/axeditorapp/trackwidget.h
@@ -3,6 +3,40 @@
 
 #include <QGraphicsView>
 
+#include "private.h"
+
+class TrackWidget;
+
+class TrackWidgetItem : public QGraphicsItem
+{
+public:
+	TrackWidgetItem();
+	TrackWidgetItem(TrackWidget* widget);
+	TrackWidgetItem(TrackWidgetItem* parent);
+	TrackWidgetItem(TrackWidgetItem* parent, Gfx::IAnimatable* anim);
+	TrackWidgetItem(TrackWidgetItem* parent, TrackWidgetItem* preceding);
+	virtual ~TrackWidgetItem();
+
+	void setAnimatable(Gfx::IAnimatable* anim);
+	Gfx::IAnimatable* getAnimatable() const;
+
+	virtual QRectF boundingRect() const;
+
+	// drawing
+	virtual void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0);
+
+protected:
+	void emitDataChanged();
+
+private:
+	TrackWidget* m_widget;
+	TrackWidgetItem* m_parent;
+	Gfx::IAnimatable* m_anim;
+	QGraphicsItem* m_graphItem;
+};
+
+
+
 class TrackWidget : public QGraphicsView
 {
 	Q_OBJECT
@@ -11,8 +45,12 @@ public:
 	TrackWidget(QWidget *parent);
 	~TrackWidget();
 
+protected:
+	virtual void resizeEvent(QResizeEvent * event);
+
 private:
-	IAnimatable* m_anim;
+	Gfx::IAnimatable* m_anim;
+	QGraphicsScene* m_scene;
 	int m_lineHeight;
 	bool m_showBar;
 };
